67aeccff5b891f1bc941492c00fe74aaf65393c7
Abhyuday Singh <TUMHARA_GITHUB_EMAIL>
2026-09-06 19:50:39 +0530

    Fix port configuration

--- app.js
+++ app.js
@@ -26,7 +26,11 @@ app.use(express.urlencoded({ extended:true}));
 app.use("/", indexRouter);
 app.use("/user", userRouter);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000");
+// })
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
 
